467a8b99355585d7aab40dfedbec818e6f4465b3
Nahiyan Alamgir <alnahiyan97@gmail.com>
2014-12-09 19:45:54 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap 101 Template</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class='container-fluid'>
+            <h2>Chat</h2>
+            <div class='chat-wrapper jumbotron'>
+                <div class=' row'>
+                    <div class='message-box col-md-9'>
+                    <div class='message-icon col-md-2'>
+                        <img src="http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png" width="50px" height="50px" />
+                    </div>
+                
+                    <div class='col-md-5'><div class='message-content'>This is some random message sent by Nahiyan.</div></div></div>
+                </div>
+                <div class='row'>
+                    <div class='message-input col-md-6'>
+                        <textarea class='form-control' placeholder="Enter some sexy text."></textarea>
+                        <button class='btn btn-primary'>Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
